0069989f4ddc0e7eada8afc4a1a1540a25a75fd4
APP MUNDIAL MANATURY <ecobienestarsanluis@gmail.com>
2026-01-22 05:53:06 +0100

    Update index.tsx
    Tu App.tsx debe ser un componente puro, sin lógica de montaje.

--- index.tsx
+++ index.tsx
@@ -3,9 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const App: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Login />
+      {/* otras secciones */}
+    </>
+  );
+};
+
+export default /App;
+
 }
 
 const root = ReactDOM.createRoot(rootElement);
